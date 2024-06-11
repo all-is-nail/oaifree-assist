85b959bbf5109c704ae67d30bb9878d58ba543c3
m0v1 <zheng9feng@vip.qq.com>
2024-06-12 02:15:24 +0800

    local commit

---
+++ src/main/java/org/example/oaifreeassist/OaiFreeAssistApplication.java
@@ -0,0 +1,13 @@
+package org.example.oaifreeassist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OaiFreeAssistApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OaiFreeAssistApplication.class, args);
+    }
+
+}
