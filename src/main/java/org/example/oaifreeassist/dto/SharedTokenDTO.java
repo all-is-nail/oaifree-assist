83f10688f2dc73f6299d045515fc23f181b39103
m0v1 <mail@chatgpt-x.plus>
2024-06-12 18:03:11 +0800

    feat: 新增模型

---
+++ src/main/java/org/example/oaifreeassist/dto/SharedTokenDTO.java
@@ -0,0 +1,20 @@
+package org.example.oaifreeassist.dto;
+
+import lombok.Data;
+
+@Data
+public class SharedTokenDTO {
+    // private String user_id;
+    // private String user_email;
+    private String unique_name;
+    private String access_token;
+    // private String token_key;
+    private Integer expires_in;
+    // private Integer expires_at;
+    private String site_limit;
+    private Integer gpt35_limit;
+    private Integer gpt4_limit;
+    private Boolean show_conversations;
+    private Boolean temporary_chat;
+    private Boolean reset_limit;
+}
