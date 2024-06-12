085186937a0ad0d88fe58dde41e5db62a862c9f7
m0v1 <zheng9feng@vip.qq.com>
2024-06-12 21:12:21 +0800

    style: reformat

--- src/main/java/org/example/oaifreeassist/dto/SharedTokenResponse.java
+++ src/main/java/org/example/oaifreeassist/dto/SharedTokenResponse.java
@@ -7,26 +7,37 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Data
 public class SharedTokenResponse {
+
     @JsonProperty("expire_at")
     private Integer expireAt;
+
     @JsonProperty("gpt35_limit")
     private Integer gpt35Limit;
+
     @JsonProperty("gpt4_limit")
     private Integer gpt4Limit;
+
     @JsonProperty("reset_limit")
     private Boolean resetLimit;
+
     @JsonProperty("show_conversations")
     private Boolean showConversations;
+
     @JsonProperty("site_limit")
     private String siteLimit;
+
     @JsonProperty("temporary_chat")
     private Boolean temporaryChat;
+
     @JsonProperty("token_key")
     private String tokenKey;
+
     @JsonProperty("unique_name")
     private String uniqueName;
+
     @JsonProperty("user_email")
     private String userEmail;
+
     @JsonProperty("user_id")
     private String userId;
 }
