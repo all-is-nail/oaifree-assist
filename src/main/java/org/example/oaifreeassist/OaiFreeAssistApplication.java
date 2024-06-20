13b64cf5cc9020c5c37814d66c765bdffd2de206
m0v1 <zheng9feng@vip.qq.com>
2024-06-21 00:49:27 +0800

    feat: 开启事务管理

--- src/main/java/org/example/oaifreeassist/OaiFreeAssistApplication.java
+++ src/main/java/org/example/oaifreeassist/OaiFreeAssistApplication.java
@@ -2,7 +2,9 @@ package org.example.oaifreeassist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+@EnableTransactionManagement
 @SpringBootApplication
 public class OaiFreeAssistApplication {
 
