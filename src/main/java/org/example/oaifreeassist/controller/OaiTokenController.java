5bf1d58a53087308d31e580a43152b5c0178d452
m0v1 <zheng9feng@vip.qq.com>
2024-06-24 01:14:35 +0800

    refactor: rename

--- src/main/java/org/example/oaifreeassist/controller/OaiTokenController.java
+++ src/main/java/org/example/oaifreeassist/controller/OaiTokenController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/tokens")
-public class TokenController {
+public class OaiTokenController {
 
     @Autowired
     private OaiTokenManagementService oaiTokenManagementService;
