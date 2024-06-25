a9a58a2a6977e77c39d8f627df3ec4a18456c8b1
m0v1 <zheng9feng@vip.qq.com>
2024-06-26 01:07:33 +0800

    feat: enable schedule

--- src/main/java/org/example/oaifreeassist/OaiFreeAssistApplication.java
+++ src/main/java/org/example/oaifreeassist/OaiFreeAssistApplication.java
@@ -2,8 +2,10 @@ package org.example.oaifreeassist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+@EnableScheduling
 @EnableTransactionManagement
 @SpringBootApplication
 public class OaiFreeAssistApplication {
