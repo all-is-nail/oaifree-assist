fcff07167159ace343cea4fdcf2cc934233ce0e8
m0v1 <mail@chatgpt-x.plus>
2024-06-25 09:22:14 +0800

    feat: share management

---
+++ src/main/java/org/example/oaifreeassist/mapper/OaiSharedManagementMapper.java
@@ -0,0 +1,18 @@
+package org.example.oaifreeassist.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.example.oaifreeassist.entity.OaiSharedManagement;
+
+/**
+ * @author m0v1
+ * @description 针对表【oai_shared_management(共享账号管理)】的数据库操作Mapper
+ * @createDate 2024-06-24 13:00:16
+ * @Entity org.example.oaifreeassist.entity.OaiSharedManagement
+ */
+public interface OaiSharedManagementMapper extends BaseMapper<OaiSharedManagement> {
+
+}
+
+
+
+
