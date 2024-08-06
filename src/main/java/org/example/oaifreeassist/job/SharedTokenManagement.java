a2d17f295d59510037e59de2e83978eb19dc3a88
m0v1 <mail@mail>
2024-08-06 14:58:20 +0800

    refactor: update logic

--- src/main/java/org/example/oaifreeassist/job/SharedTokenManagement.java
+++ src/main/java/org/example/oaifreeassist/job/SharedTokenManagement.java
@@ -34,12 +34,15 @@ public class SharedTokenManagement {
     public void renewSharedToken() {
         log.info("Renew shared token");
         // TODO
-        // 查询处于有效期的 st
+        // 查询处于有效期的 st（一天内到期）
         LambdaQueryWrapper<OaiSharedManagement> queryWrapper = Wrappers.lambdaQuery(OaiSharedManagement.class);
         LocalDateTime now = LocalDateTime.now();
-        queryWrapper.gt(OaiSharedManagement::getExpirationTime, now);
+        // 限制条件：一天内到期
+        queryWrapper.gt(OaiSharedManagement::getExpirationTime, now)
+                .lt(OaiSharedManagement::getExpirationTime, now.plusDays(1L));
         long validOaiShareTokenCount = oaiSharedManagementService.count(queryWrapper);
         if (validOaiShareTokenCount < 1) {
+            log.warn("未找到将在一天内到期的st，本次续期操作执行结束！");
             return;
         }
 
@@ -49,10 +52,15 @@ public class SharedTokenManagement {
 
             // 判断对应的 ac 是否处于有效期
             OaiTokenManagement accessTokenInfo = oaiTokenManagementService.getById(tokenId);
+            if (accessTokenInfo == null) {
+                log.warn("当前st:{}未找到对应的ac，本次续期操作执行结束！", oaiSharedManagement.getShareToken());
+                return;
+            }
             // |__ if ac 有效，则执行 st 续期
-            if (accessTokenInfo != null && accessTokenInfo.getExpireTime().isAfter(now)) {
+            if (accessTokenInfo.getExpireTime().isAfter(now)) {
                 oaiSharedManagementService.renewoaisharedmanagement(oaiSharedManagementId, accessTokenInfo.getTokenValue());
-
+                // 更新续期后的 st
+                oaiSharedManagementService.updateById(oaiSharedManagement);
                 // |__ not 查询 ac 对应的账户是否存在 rt
             } else {
                 LambdaQueryWrapper<OaiTokenManagement> oaiTokenManagementWrapper = Wrappers.lambdaQuery(OaiTokenManagement.class);
@@ -62,12 +70,14 @@ public class SharedTokenManagement {
                 // |____ 存在
                 // |______则根据 rt 获取新的 ac，更新当前 st 对应的 ac 并执行 st 续期
                 if (CollectionUtils.isNotEmpty(oaiRefreshToken)) {
-                    String refreshTokenValue = oaiRefreshToken.get(0).getTokenValue();
+                    OaiTokenManagement oaiTokenManagement = oaiRefreshToken.get(0);
+                    String refreshTokenValue = oaiTokenManagement.getTokenValue();
                     String newAccessToken = accessTokenService.getAccessTokenByRefreshToken(refreshTokenValue);
                     // 更新 ac
-
+                    oaiTokenManagement.setTokenValue(newAccessToken);
+                    oaiTokenManagementService.updateById(oaiTokenManagement);
                     // 续期
-                    oaiSharedManagementService.renewoaisharedmanagement(oaiSharedManagementId, null);
+                    oaiSharedManagementService.renewoaisharedmanagement(oaiSharedManagementId, newAccessToken);
 
                 }
                 // |____ 不存在，不处理当前 st 的续期
